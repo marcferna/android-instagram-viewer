f0a06ddb8c3bafbcda6be84ec6b82d4544dee950
Marc Fernandez <marc.fernandezg@gmail.com>
2014-09-13 17:37:36 -0700

    Created Instagram Photo model

--- app/src/main/java/com/marcferna/instagramclient/InstagramClient.java
+++ app/src/main/java/com/marcferna/instagramclient/InstagramClient.java
@@ -4,8 +4,11 @@ import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.marcferna.models.InstagramPhoto;
 
 import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -28,6 +31,23 @@ public class InstagramClient {
       @Override
       public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
         Log.i("INFO", response.toString());
+        JSONArray photosJSONArray = null;
+        try {
+          photosJSONArray = response.getJSONArray("data");
+          for (int i = 0; i < photosJSONArray.length(); i++) {
+            JSONObject photoJSON = photosJSONArray.getJSONObject(i);
+            InstagramPhoto photo = new InstagramPhoto();
+            photo.username = photoJSON.getJSONObject("user").getString("username");
+            photo.caption = photoJSON.getJSONObject("caption").getString("text");
+            JSONObject standardResolutionImage = photoJSON.getJSONObject("images").getJSONObject("standard_resolution");
+            photo.url = standardResolutionImage.getString("url");
+            photo.height = standardResolutionImage.getInt("height");
+            photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");
+
+          }
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
       }
 
       @Override
---
+++ app/src/main/java/com/marcferna/models/InstagramPhoto.java
@@ -0,0 +1,12 @@
+package com.marcferna.models;
+
+/**
+ * Created by marc on 9/13/14.
+ */
+public class InstagramPhoto {
+  public String username;
+  public String caption;
+  public String url;
+  public int height;
+  public int likesCount
+}
